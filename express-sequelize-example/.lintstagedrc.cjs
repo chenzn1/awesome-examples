252eb881e3a71f19a9e29b126e9aba747c67010e
chenzn1 <312073339@qq.com>
2024-01-12 11:04:51 +0800

    refactor: folder

--- express-sequelize-example/.lintstagedrc.cjs
+++ express-sequelize-example/.lintstagedrc.cjs
@@ -2,7 +2,7 @@ const path = require('path')
  
 const buildEslintCommand = (filenames) => 
   `eslint --fix ${filenames
-    .map((f) => path.relative('examples/express-sequelize-example', f))
+    .map((f) => path.relative('express-sequelize-example', f))
     .join(' ')}`
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
--- nextjs-prisma-graphql-example/.lintstagedrc.js
+++ nextjs-prisma-graphql-example/.lintstagedrc.js
@@ -2,7 +2,7 @@ const path = require('path')
  
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
-    .map((f) => path.relative('examples/nextjs-prisma-graphql-example', f))
+    .map((f) => path.relative('nextjs-prisma-graphql-example', f))
     .join(' --file ')}`
  
 module.exports = {
